246b830b627bd8f607ea87d9a2d0dd8cdb9497c1
tastenov <160467529+tastenov@users.noreply.github.com>
2024-04-20 10:08:39 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ dconf write /org/mate/terminal/profiles/default/background-type "'solid'"
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ foreground-color '#8F8FF0F0A4A4'
 gsettings set org.mate.background picture-filename '/usr/share/backgrounds/hackthebox.jpg'
 
-echo -e '\nPROMPT_COMMAND='\''PS1_CMD1=$(ip route get 10.10.14.1 | awk -F"src " '"'"'"'"'"'"'"'NR == 1{ split($2, a," ");print a[1]}'"'"'"'"'"'"'"')'\''; PS1='\''\n\[\e[38;5;32m\]┌─\[\e[38;5;33m\][\[\e[38;5;75m\]\u\[\e[38;5;221m\]@\[\e[38;5;77m\]${PS1_CMD1}\[\e[38;5;33m\]]\[\e[38;5;32m\]-\[\e[38;5;33m\][\[\e[0m\]\w\[\e[38;5;33m\]]\n\[\e[38;5;32m\]└──╼ \[\e[38;5;215m\]\$\[\e[0m\] '\'' >> ~/.bashrc && source ~/.bashrc
+timedatectl set-timezone Asia/Qyzylorda
 
 echo "set -g mouse on" > ~/.tmux.conf
