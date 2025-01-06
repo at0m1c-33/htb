10e9c24b9300a15dae02fb1d5ebbae865e11759a
at0m1c-33 <160467529+at0m1c-33@users.noreply.github.com>
2025-01-06 19:39:11 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo -e "set -g mouse on\nset -g history-limit 10000\nset -g default-command /bin/bash" >> ~/.tmux.conf
+curl https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux > ~/.bash_completion
 
 sudo apt-get update && sudo apt-get install -y alacritty tree
 mkdir -p ~/.config/alacritty && echo -e 'colors:\n  primary:\n    background: "#141D2B"' > ~/.config/alacritty/alacritty.yml
