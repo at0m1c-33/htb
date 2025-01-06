7f9e74d7dcd9ccd7c45c1cf4874bad489585e7b3
at0m1c-33 <160467529+at0m1c-33@users.noreply.github.com>
2025-01-06 19:44:10 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Tmux Setup Mouse Support, History Limit and Disable Startup Prompt
 echo -e "set -g mouse on\nset -g history-limit 10000\nset -g default-command /bin/bash" >> ~/.tmux.conf
-curl https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux > ~/.bash_completion
 
+# Tmux Bash Completition
+curl -fSsL "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux" > ~/.bash.tmux-bash-completion
+echo 'source ~/.bash.tmux-bash-completion' >> ~/.bashrc
+
+# Install Alacritty and Change Background Color
 sudo apt-get update && sudo apt-get install -y alacritty tree
 mkdir -p ~/.config/alacritty && echo -e 'colors:\n  primary:\n    background: "#141D2B"' > ~/.config/alacritty/alacritty.yml
 
+# Add tmux and Alacritty to $TERM variable for persistent prompt
 sed -i '/case "\$TERM" in/{n;s/xterm\*|rxvt\*/xterm*|rxvt*|tmux*|alacritty*/}' ~/.bashrc
 source ~/.bashrc
 
+# Create Alacritty Shortcut on the Desktop with Always Maximized Window mode
 echo -e '#!/usr/bin/env xdg-open\n[Desktop Entry]\nType=Application\nTryExec=alacritty\nExec=alacritty --option window.startup_mode=Maximized\nIcon=Alacritty\nTerminal=false\nCategories=System;TerminalEmulator;\nName=Alacritty\nGenericName=Terminal\nComment=A fast, cross-platform, OpenGL terminal emulator\nStartupWMClass=Alacritty\nActions=New;\n\n[Desktop Action New]\nName=New Terminal\nExec=alacritty --option window.startup_mode=Maximized' > ~/Desktop/alacritty.desktop && chmod +x ~/Desktop/alacritty.desktop
