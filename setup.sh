f7f2d1930e597b97e19b2c5143e2f31ebbb37452
at0m1c-33 <160467529+at0m1c-33@users.noreply.github.com>
2025-01-18 08:59:08 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
-# Tmux Setup Mouse Support, History Limit, Disable Startup Prompt, Always start at home directory
-echo -e "set -g mouse on\nset -g history-limit 10000\nset -g default-command /bin/bash\nbind c new-window -c ~\nbind % split-window -h -c ~\nbind '"' split-window -v -c ~" >> ~/.tmux.conf
+# Tmux Setup: Mouse support, history limit, disable startup prompt, always start at home directory
+echo -e 'set -g mouse on
+set -g history-limit 10000
+set -g default-command /bin/bash
+bind c new-window -c ~
+bind % split-window -h -c ~
+bind "\"" split-window -v -c ~' >> ~/.tmux.conf
 
-# Tmux Bash Completition
-curl -fSsL "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux" > ~/.bash.tmux-bash-completion
+# Tmux Bash Completion
+curl -fSsL "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux" \
+  > ~/.bash.tmux-bash-completion
 echo 'source ~/.bash.tmux-bash-completion' >> ~/.bashrc
 
-# Install Alacritty and Change Background Color
+# Install Alacritty and tree, set Alacritty background
 sudo apt-get update && sudo apt-get install -y alacritty tree
-mkdir -p ~/.config/alacritty && echo -e 'colors:\n  primary:\n    background: "#141D2B"' > ~/.config/alacritty/alacritty.yml
+mkdir -p ~/.config/alacritty
+echo -e 'colors:\n  primary:\n    background: "#141D2B"' > ~/.config/alacritty/alacritty.yml
 
-# Add tmux and Alacritty to $TERM variable for persistent prompt
-sed -i '/case "\$TERM" in/{n;s/xterm\*|rxvt\*/xterm*|rxvt*|tmux*|alacritty*/}' ~/.bashrc
+# Modify $TERM for tmux and Alacritty
+sed -i '/case "\$TERM" in/{n;s/xterm\*|rxvt\*/xterm\*|rxvt\*|tmux\*|alacritty\*/}' ~/.bashrc
+
+# Reload bashrc (affects only *this* shell while script runs)
 source ~/.bashrc
 
-# Create Alacritty Shortcut on the Desktop with Always Maximized Window mode
-echo -e '#!/usr/bin/env xdg-open\n[Desktop Entry]\nType=Application\nTryExec=alacritty\nExec=alacritty --option window.startup_mode=Maximized\nIcon=Alacritty\nTerminal=false\nCategories=System;TerminalEmulator;\nName=Alacritty\nGenericName=Terminal\nComment=A fast, cross-platform, OpenGL terminal emulator\nStartupWMClass=Alacritty\nActions=New;\n\n[Desktop Action New]\nName=New Terminal\nExec=alacritty --option window.startup_mode=Maximized' > ~/Desktop/alacritty.desktop && chmod +x ~/Desktop/alacritty.desktop'
+# Create Alacritty Desktop shortcut
+echo -e '#!/usr/bin/env xdg-open
+[Desktop Entry]
+Type=Application
+TryExec=alacritty
+Exec=alacritty --option window.startup_mode=Maximized
+Icon=Alacritty
+Terminal=false
+Categories=System;TerminalEmulator;
+Name=Alacritty
+GenericName=Terminal
+Comment=A fast, cross-platform, OpenGL terminal emulator
+StartupWMClass=Alacritty
+Actions=New;
+
+[Desktop Action New]
+Name=New Terminal
+Exec=alacritty --option window.startup_mode=Maximized' \
+> ~/Desktop/alacritty.desktop
+
+chmod +x ~/Desktop/alacritty.desktop
